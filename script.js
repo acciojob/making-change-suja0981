2a3b1e66fc16d3995d7d563a93ac99e86a5c7484
suja0981 <147859989+suja0981@users.noreply.github.com>
2026-07-29 23:33:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const makeChange = (c) => {
+const makeChange = (amount) => {
   // your name here
+	
+    const change = {};
+
+    change.q = Math.floor(amount / 25);
+    amount %= 25;
+
+    change.d = Math.floor(amount / 10);
+    amount %= 10;
+
+    change.n = Math.floor(amount / 5);
+    amount %= 5;
+
+    change.p = amount;
+
+    return change;
 };
 
 // Do not the change the code below
